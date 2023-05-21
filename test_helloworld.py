94a4456cdfd8a3dfa53c37ec2ae3d5f7a68b385b
FranciscoCosta28 <127154838+FranciscoCosta28@users.noreply.github.com>
2023-05-21 23:03:07 +0200

    Update test_helloworld.py

--- test_helloworld.py
+++ test_helloworld.py
@@ -1,4 +1,6 @@
-def test_somar():
-    assert somar(2, 3) == 5
-    assert somar(-1, 5) == 4
-    assert somar(0, 0) == 0
+import helloworld
+
+    if helloworld.somar(2,3) == 5 :
+        print("ok")
+    else:
+        print("not ok")
